c1b6840602430510d39b95631eeae7d57f8b9ecc
storm246 <alegosdude@gmail.com>
2018-04-19 18:59:00 -0400

    fixed errors and added strings.

--- StormScript/StormScript.cpp
+++ StormScript/StormScript.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <cctype>
 
 using std::string;
 using std::cout;
@@ -11,10 +12,13 @@ class sts
 {
 	int lineon;
 	unsigned int sizeoff = 0;
-	void error(int num);
-	string svar_name = "";
-	string svar_cont = "";
-	int var_num = 0;
+	void error(int num, string issue);
+	string svar_name = ""; //name of string
+	string ivar_name = ""; //name of integer
+	string svar_cont = ""; //contents of string
+	int ivar_cont; //contents of integer
+	string sivar_cont; //contents of ivar as string
+	int var_num = 0; //number of variables
 public:
 	string line;
 	void out(string str);
@@ -64,7 +68,7 @@ void sts::read() {
 		prg[size(prg)-1] += contents[x];
 	} //add last line to vector
 
-	string progm[10000];
+	string progm[10000]; //will change to something that doesn't limit lines
 
 	for (int x = 0; x <= sizeoff-1; x++) {
 		progm[x] = prg[x];
@@ -74,25 +78,29 @@ void sts::read() {
 }
 
 void sts::readline(string prg[],int big) {
-	std::vector<string> svars(big + 1);
+	std::vector<string> vars(big + 1);
 
 	for (int x = 0; x <= big; x++) 
 	{
 		string l = prg[x];
-		string svarval;
+		string varval;
+
 		for (int y = 0; y < size(l); y++) {
-			if ((l[y] == 'o') && (l[y + 1] == 'u') && (l[y + 2] == 't')) {
-				int z = y + 4;
-				string str="";
-				string strvarnm = "";
+			int z = y + 4;
+			if ((l[y] == 'o') && (l[y + 1] == 'u') && (l[y + 2] == 't')) { //checks if command is out
+				string str=""; //string to print
+				string varnm = ""; //variable name to print
 
 				while (l[z] != ';') {
 					if (l[z] == '"') {
+
 						z++;
+
 						while (l[z] != '"') {
 							str += l[z];
 							z++;
 						}
+
 						z++;
 						out(str);
 					}
@@ -101,74 +109,144 @@ void sts::readline(string prg[],int big) {
 						int n = y + 4;
 
 						while (l[n] != ';') {
-							strvarnm += l[n];
+							varnm += l[n];
 							n++;
 						}
 
-						string svarname;
+						string varname;
 						int b = 0;
-						while (svarname != strvarnm) {
+						while (varname != varnm) {
+							string var = ""; //variable contents
 
-							string svar = svars[b];
-							svarname = "";
-							svarval = "";
+							try { //checks if svars has position b
+								var = vars.at(b);
+								var = vars[b];
+							}
+							catch (const std::out_of_range& m) { //catches error if it doesn't
+								error(0, varnm);
+							}
+
+							varname = "";
+							varval = "";
 							int a = 0;
+							try { //out str
+								while (var[a] != ':') {
+									varname += var[a];
+									a++;
+								}
 
-							while (svar[a] != ':') {
-								svarname += svar[a];
 								a++;
-							}
 
-							a++;
+								while (var[a] != '"') {
+									varval += var[a];
+									a++;
+									if (var[a] == ';') {
+										a = 0;
+										varname = "";
+										varval = "";
+										throw std::out_of_range("int");
+									}
+								}
 
-							while (svar[a] != '"') {
-								svarval += svar[a];
-								a++;
 							}
-							b++;
+							catch (const std::out_of_range& e) { //out int
+								try {
+									while (var[a] != ':') {
+										varname += var[a];
+										a++;
+									}
+									a++;
+
+									while (var[a] != ';') {
+										varval += var[a];
+										a++;
+									}
+								}
+								catch (const std::out_of_range& l) {
+									error(0, varnm);
+								}
+							}
+						b++;
 						}
-						str = svarval;
+						str = varval;
 						out(str);
 						break;
 					}
 				}
 			}
-			else if ((l[y] == 's') && (l[y + 1] == 't') && (l[y + 2] == 'r')) {
+			else if ((l[y] == 's') && (l[y + 1] == 't') && (l[y + 2] == 'r')) { //if str
 				svar_cont = "";
 				svar_name = "";
-				int a = y + 4;
-				while (l[a] != ':') {
-					svar_name += l[a];
-					a++;
+
+				while (l[z] != ':') {
+					svar_name += l[z]; //add char to var name
+					z++;
 				}
-				a += 2;
-				if (svar_name != "") {
-					if (l[a] == '"') {
-						a++;
-						while (l[a] != '"'){
-							svar_cont += l[a];
-							a++;
+
+				z += 2;
+
+				if (svar_name != "") { //if name isn't blank
+					if (l[z] == '"') {
+						z++;
+
+						while (l[z] != '"'){
+							svar_cont += l[z]; //l[z] to variable cont
+							z++;
 						}
-						svar_cont += l[a];
+
+						svar_cont += l[z];
+					
 					}
-					svars.resize(var_num+1);
-					svars[var_num] += svar_name+":"+svar_cont;
+					
+					vars.resize(var_num+1);
+					vars[var_num] += svar_name+":"+svar_cont;
 					var_num++;
 				}
 				else {
-					error(1);
+					error(1, "");
 				}
 			}
 			else if ((l[y] == 'i') && (l[y + 1] == 'n') && (l[y + 2] == 't')) {
-				cout << "cool, a new feature!" << endl;
+				ivar_name = "";
+				string ivar_conts = "";
+
+				while (l[z] != ':') {
+					ivar_name += l[z];
+					z++;
+				}
+
+				z += 2;
+				if (ivar_name != "") {
+					while (l[z] != ';') {
+						ivar_conts += l[z];
+						z++;
+					}
+					ivar_conts += l[z];
+					for (int m = 0; m <= size(ivar_conts)-1; m++) {
+						if ((std::isdigit(ivar_conts[m]) / 4 == true) or (ivar_conts[m]==';')) {
+							sivar_cont += ivar_conts[m];
+						}
+						else {
+							error(3, ivar_name);
+						}
+					}
+
+					vars.resize(var_num+1);
+					vars[var_num] = ivar_name + ":" + sivar_cont;
+					var_num++;
+
+				}
+				else {
+					error(2, "");
+				}
 			}
 		}
 	}
 }
 
-void sts::error(int num) {
+void sts::error(int num, string issue = "") {
 	if (num == 0) {
-		cout << "Error: Variable Undefined" << endl;
+		cout << "Error: No variable named \"" << issue << "\"." << endl;
 	}
 	else if (num == 1) {
 		cout << "Error: string variable must have name" << endl;
@@ -176,7 +254,11 @@ void sts::error(int num) {
 	else if (num == 2) {
 		cout << "Error: integer variable must have name" << endl;
 	}
-
+	else if (num == 3) {
+		cout << "Error: integer \"" << issue << "\" must be a number" << endl;
+	}
+	cout << "Press any key to exit...";
+	getchar();
 	exit(0);
 }
 
