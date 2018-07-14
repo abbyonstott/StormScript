3d0c431e1bd07a653f6d947750bdb45377b8fbc5
storm246 <alegosdude@gmail.com>
2018-07-13 20:07:25 -0400

    fixed eof parsing

--- StormScript/compile.cpp
+++ StormScript/compile.cpp
@@ -1,5 +1,9 @@
 #include "stsclasses.h" 
 
+/*
+the compiler parses the file and calls functions in other files
+*/
+
 std::vector<string> sts::parse(std::vector<string> prg){
     std::vector<string> x;
     int y = 0;
@@ -31,7 +35,7 @@ std::vector<string> sts::parse(std::vector<string> prg){
 
         y++;
     }
-
+    x[x.size()-1].pop_back(); //removes EOF char from end of file so I can parse
     return x;
 }
 
@@ -39,37 +43,29 @@ void sts::compile(string fname, std::vector<string> prg, int psize){
     prs = parse(prg);
 
     system("compilecheck.sh");
-    system("echo '#include <iostream>\\nint main(){' > stscompile/stscomp.cpp");
-
-    for (int x = 0; x<=prs.size()-1; x++){
-        string name = "";
-        string val = "";
-        
-        if ((prs[x]=="\tout") || (prs[x]=="out")){
-            for (int y=0; y<=prs[x+1].size();y++){
-                if (prs[x+1][y]!=';'){
-                    val+=prs[x+1][y];
+    system("echo '#include <iostream>' > stscompile/stscomp.cpp");
+    system("echo 'int main(){' >> stscompile/stscomp.cpp");
+    for (int x = 0; x<=prs.size(); x++){
+        if (prs[x]=="do{"){
+            int y = x;
+            while (true){
+                if ((prs[y]=="\tout") || (prs[y]=="out")){
+                    system(out(y).c_str());
                 }
+                else if ((prs[y]=="\tstr") || (prs[y]=="str")){
+                    system(declare('s',y).c_str());
+                }
+                else if ((prs[y]=="\tint") || (prs[y]=="int")){
+                    system(declare('i',y).c_str());
+                }
+                else if (prs[y]=="}end;"){
+                    system("echo '\treturn 0;\n}' >> stscompile/stscomp.cpp");
+                    break;
+                }
+                y++;
             }
-
-            string a = "echo '\tstd::cout << "; 
-            const char *c = " << std::endl;' >> stscompile/stscomp.cpp";
-            a+=val.c_str();
-            a+=c;
-            const char *prnc = a.c_str(); //kind of a hack, but it works. Makes c_str and concatinates.
-
-            system(prnc);
         }
-        else if ((prs[x]=="\tstr") || (prs[x]=="str")){
-            system(declare('s',x).c_str());
-        }
-        else if ((prs[x]=="\tint") || (prs[x]=="int")){
-            system(declare('i',x).c_str());
-        }
-    }
-
-    system("echo '\treturn 0;\\n}\\n' >> stscompile/stscomp.cpp");
-
+    }   
     system("g++ stscompile/stscomp.cpp -o stsprg.out");
 
     cout << "Successfully Compiled" << endl;
