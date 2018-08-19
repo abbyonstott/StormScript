729de716ae06e66e2f917b4c6c4384a69a7d829b
ethan <alegosdude@gmail.com>
2018-08-19 15:41:09 -0400

    windows version of helloworld lib

--- libraries/helloworldpy/helloworldpy.cpp
+++ libraries/helloworldpy/helloworldpy.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+#if defined(_WIN32)
+#define PLATFORM "Windows"
+#else
+#define PLATFORM "other"
+#endif
+
 int main(int argc, char *argv[]){
-    std::string cmd1 = "helloworldstslib.py ";
+    std::string cmd1;
+    if (PLATFORM == "Windows"){
+        cmd1 = "python helloworldstslibwin.py ";
+    }
+    else{
+        cmd1 = "helloworldstslib.py ";
+    }
     cmd1+=" ";
     cmd1+=argv[1];
     system(cmd1.c_str());
