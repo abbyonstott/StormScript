16ece3ef468f3cc396312d4decc77b4d91c36a4e
ethan <alegosdude@gmail.com>
2018-08-23 21:21:12 -0400

    reformatting dirs

---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this builds stormscript for linux and windows.
+g++ lang/parse.cpp lang/sts.cpp lang/stsdec.cpp lang/stsstream.cpp -o release/linux-x86/bin/sts
+g++ libraries/helloworldpy/helloworldpy.cpp -o release/linux-x86/libs/helloworldpy.stslib
+g++ libraries/helloworldlib/helloworld.cpp -o release/linux-x86/libs/helloworld.stslib
+tar -czvf release/linux-x86_64.tar.gz release/linux-x86
+x86_64-w64-mingw32-g++ lang/parse.cpp lang/sts.cpp lang/stsdec.cpp lang/stsstream.cpp -o release/windows-x86/bin/sts.exe -static-libgcc -static-libstdc++
+x86_64-w64-mingw32-g++ libraries/helloworldpy/helloworldpy.cpp -o release/windows-x86/bin/helloworldpystslib.exe -static-libgcc -static-libstdc++
+x86_64-w64-mingw32-g++ libraries/helloworldlib/helloworld.cpp -o release/windows-x86/bin/helloworldstslib.exe -static-libgcc -static-libstdc++
+zip -r release/windows-x86_64.zip release/windows-x86
---
+++ release/linux-x86/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ `whoami` == 'root' ]; then
+    cp bin/sts /usr/bin
+    cmd1='export PATH='
+    cmd2='/libs:$PATH'
+    cmd=$cmd1`pwd`$cmd2
+    echo $cmd >> ~/.bashrc
+    echo test: `sts example/helloworld.sts`
+else
+    echo Please run again as root
+fi
