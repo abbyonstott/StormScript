da6124a36b8e2caca8c288e5205f4e099c3c377d
ethan <alegosdude@gmail.com>
2018-08-25 21:59:00 -0400

    prepare for release

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@ g++ libraries/helloworldpy/helloworldpy.cpp -o release/linux-x86/libs/helloworld
 g++ libraries/helloworldlib/helloworld.cpp -o release/linux-x86/libs/helloworld.stslib
 tar -czvf release/linux-x86_64.tar.gz release/linux-x86
 x86_64-w64-mingw32-g++ lang/parse.cpp lang/sts.cpp lang/stsdec.cpp lang/stsstream.cpp -o release/windows-x86/bin/sts.exe -static-libgcc -static-libstdc++
-x86_64-w64-mingw32-g++ libraries/helloworldpy/helloworldpy.cpp -o release/windows-x86/libs/helloworldpystslib.exe -static-libgcc -static-libstdc++
-x86_64-w64-mingw32-g++ libraries/helloworldlib/helloworld.cpp -o release/windows-x86/libs/helloworldstslib.exe -static-libgcc -static-libstdc++
+x86_64-w64-mingw32-g++ libraries/helloworldpy/helloworldpy.cpp -o release/windows-x86/bin/helloworldpystslib.exe -static-libgcc -static-libstdc++
+x86_64-w64-mingw32-g++ libraries/helloworldlib/helloworld.cpp -o release/windows-x86/bin/helloworldstslib.exe -static-libgcc -static-libstdc++
 zip -r release/windows-x86_64.zip release/windows-x86
