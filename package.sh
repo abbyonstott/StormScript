ae2578efde47bd9fc514563828a3fab25fde7823
storm246 <alegosdude@gmail.com>
2018-10-31 21:18:05 -0400

    package file

---
+++ package.sh
@@ -0,0 +1,10 @@
+rm -r release/*
+
+mkdir release/src
+
+cp -r classes release/src
+cp -r stream release/src
+cp -r core release/src
+cp -r values release/src
+cp -r scripts/* release
+cp StormScriptconfig.h release/src
