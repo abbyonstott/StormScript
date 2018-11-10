28305eb02bf76bd16d7d51dea42c6679f1410311
storm246 <alegosdude@gmail.com>
2018-11-09 21:21:41 -0500

    new variable and literal handling works with print

--- values/getvals.cpp
+++ values/getvals.cpp
@@ -1,6 +1,6 @@
 #include "../core/stsclasses.h"
 
-stsvars sts::getval(std::vector<string> prs, int *line) {
+stsvars sts::getval(std::vector<string> prs, std::vector<stsvars> vars, int *line) {
     stsvars v;
     int y = *line;
 
@@ -20,6 +20,19 @@ stsvars sts::getval(std::vector<string> prs, int *line) {
     else if ((prs[y] == "true") || (prs[y]=="false")) {
         v.type = 'b';
         v.val = (prs[y]=="true");
+    }
+    else {
+        for (int x = 0; x<vars.size(); x++) {
+            if (vars[x].name==prs[y]) {
+                v = vars[x];
+            }
+        }
+        for (int x = 0; x<functions.size(); x++) {
+            if (functions[x].name==prs[y]) {
+                exec(functions[x].linestarted, x);
+                v = functions[x];
+            }
+        }
     }
 
     *line = y;
