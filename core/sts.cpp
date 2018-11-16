138427f15b760790181a977fa7d2bb59fb538b57
storm246 <alegosdude@gmail.com>
2018-11-16 08:48:34 -0500

    updated versioning

--- core/sts.cpp
+++ core/sts.cpp
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]) {
 
 	if (argc != 1) {
 		if (string(argv[1])=="--version"){
-			cout << "StormScript v1.2.0-alpha \"Apple 14\"" << endl;
+			cout << "StormScript v0.1.1-alpha \"Apple\"" << endl;
 		}
 		else if ((string(argv[1])=="--help") || (string(argv[1])=="-h")) {
 			cout << "Usage: stormscript [file|options]\n";
