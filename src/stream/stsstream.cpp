f3c6266085215a303be045a9341d1ad81ef3253c
storm246 <alegosdude@gmail.com>
2018-11-28 17:13:27 -0500

    fixed issues with variables in sys command

--- src/stream/stsstream.cpp
+++ src/stream/stsstream.cpp
@@ -3,24 +3,28 @@
 void sts::sys(int *y, std::vector<stsvars> vars){
     int line = *y;
     line++;
-    if (prs[line][0]=='"'){
+
+    if (prs[line][0]=='"') {
         string cmd = prs[line];
         cmd.pop_back();
         cmd.erase(cmd.begin());
         system(cmd.c_str());
     }
     else{
-        for (int x = 0; x<=vars.size()-1; x++) {
-            if (vars[x].type=='s') {
-                string val = vars[x].valstring;
-                system(val.c_str());
-            }
-            else if (vars[x].type=='i'){
-                error(10, "int");
-            }
-            else if (vars[x].type == 'b'){
-                error(10, "bool");
+        for (int x = 0; x<vars.size(); x++) {
+            if (vars[x].name==prs[line]) {
+                if (vars[x].type=='s') {
+                    string val = vars[x].valstring;
+                    system(val.c_str());
+                    line++;
+                }
+                else if (vars[x].type=='i')
+                    error(10, "int");
+
+                else if (vars[x].type == 'b')
+                    error(10, "bool");
             }
         }
     }
+    *y = line;
 }
