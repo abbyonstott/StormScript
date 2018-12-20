55abad9e302da9a289428d20f5bbe1578e004941
storm246 <alegosdude@gmail.com>
2018-12-19 21:56:16 -0500

    updated release packager

--- packagerelease.sh
+++ packagerelease.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 rm -r release/*
+echo "What is the version number (Formatted M.m.p):"
+read vnum
+
+echo "Packaging stormscript v$vnum for release."
+
+cmake CMakeLists.txt
+make
+install stormscript build/stormscript
+rm stormscript
+
+printf "\n"
+
+echo Running tests:
+cd tests
+printf "\n"
+
+B=0
+for i in $( ls | grep .sts ); do
+    # put the expected output to test
+    if [[ $(echo `cat outputs/$i.txt`) = "$(echo `../build/stormscript $i`)" ]]; then
+        echo $B: Test Successful
+    else
+        echo Test $B failed:
+        echo expected$'\n'$(echo `cat outputs/$i.txt`)$'\n'got$'\n'$(echo `../build/stormscript $i`)
+        echo "Failed to package stormscript v$vnum"
+        exit 1
+    fi
+    
+    B=$(($B+1))
+done
+
+cd ..
+
+echo "Moving files from src to release/stormscript_v$vnum"
+
 cd release
-mkdir stormscript_v0.4.0
-cd stormscript_v0.4.0
+
+mkdir stormscript_v$vnum
+cd stormscript_v$vnum
 
 cp ../../CMakeLists.txt .
 cp -r ../../src .
 cp -r ../../api .
 cp ../../build-scripts/* .
 
+echo "Compressing folder stormscript_v$vnum."
 cd ..
-tar -cf stormscript_v0.4.0.tar.xz stormscript_v0.4.0
+tar -cf stormscript_v$vnum.tar.xz stormscript_v$vnum
+
+echo "Successfully packaged stormscript_v$vnum.tar.xz"
