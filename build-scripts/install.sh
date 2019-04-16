b82b0a3f02c46297c8eebd8facbc2a3a1779ddd9
stormprograms <storm@stormprograms.com>
2019-04-16 10:37:49 -0400

    fix build scripts for new release

--- build-scripts/install.sh
+++ build-scripts/install.sh
@@ -2,6 +2,7 @@
 cmake CMakeLists.txt -DCMAKE_CXX_COMPILER:STRING="g++"
 make
 sudo cp stormscript /usr/bin
+sudo cp src/core/errors.sts /usr/bin
 
 stormscript finished.sts
 exec bash
--- build-scripts/uninstall.sh
+++ build-scripts/uninstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo rm /usr/bin/stormscript
+sudo rm /usr/bin/errors.sts
 echo StormScript Removed
