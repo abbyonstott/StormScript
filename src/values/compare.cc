7e601a123b162d7c4b1be1bdaefebb5d38c96716
Ethan Onstott <storm@stormprograms.com>
2019-05-26 20:23:17 -0400

    Update src/values/compare.cc
    Co-Authored-By: Adam Lazhar <39865721+Metr0Gn0me@users.noreply.github.com>

--- src/values/compare.cc
+++ src/values/compare.cc
@@ -11,7 +11,6 @@ bool condition(sts *program, int *y, std::vector<stsvars> *vars, std::vector<sts
     UNKNOWN | TOKEN | VALUE/UNKNOWN | TOKEN
     var     |  [    |     INTEGER   |   ]
     */
-
     TokenType comparisonType;
     int opLocation;
     int oldy = *y;
